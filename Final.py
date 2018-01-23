edfa4626007c27f0614dfc0d580ac16815131368
ahmet bozok <abozok@sfo.yearup.org>
2018-01-23 09:41:32 -0800

    second commit

--- Final.py
+++ Final.py
@@ -9,16 +9,16 @@ password = getpass.getpass("Enter your password")
 
 driver.get("https://www.office.com/")
 
-get_element_by_xpath('//*[@id="hero-banner-sign-back-in-to-office-365-link"]').click()
+find_element_by_xpath('//*[@id="hero-banner-sign-back-in-to-office-365-link"]').click()
 
-get_element_by_xpath('//*[@id="i1668"]').click()
+find_element_by_xpath('//*[@id="i1668"]').click()
 
-get_element_by_xpath('//*[@id="otherTileText"]').cick()
+find_element_by_xpath('//*[@id="otherTileText"]').cick()
 
-get_element_by_xpath('//*[@id="i0116"]').sendKeys(Keys.HOME,Keys.chord(Keys.SHIFT,Keys.END),sign)
+find_element_by_xpath('//*[@id="i0116"]').sendKeys(Keys.HOME,Keys.chord(Keys.SHIFT,Keys.END),sign)
 
-get_element_by_xpath('//*[@id="idSIButton9"]').click()
+find_element_by_xpath('//*[@id="idSIButton9"]').click()
 
-get_element_by_xpath('//*[@id="i0118"]').sendKeys(Keys.HOME,Keys.chord(Keys.SHIFT,Keys.END),password)
+find_element_by_xpath('//*[@id="i0118"]').sendKeys(Keys.HOME,Keys.chord(Keys.SHIFT,Keys.END),password)
 
 break
